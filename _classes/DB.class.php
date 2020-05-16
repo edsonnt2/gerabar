cba196a0b6072fb2ea58a555fb579c1ee251752c
Edson Rodrigo <edson_nt2@hotmail.com>
2020-05-16 13:01:24 -0300

    Add .gitignore in App

--- _classes/DB.class.php
+++ _classes/DB.class.php
@@ -5,8 +5,8 @@ class DB{
 	static function getConn(){
 		if(is_null(self::$conn) || empty(self::$conn)){
 			try{
-			$conexao = "mysql:host=localhost;dbname=geraba01_gerabar";//NOME BANCO DE DADOS: geraba01_gerabar
-			self::$conn = new PDO($conexao,"root","");//USUARIO: geraba01_admin SENHA: a1b2c3gerabar
+			$conexao = "mysql:host=localhost;dbname=geraba01_gerabar";
+			self::$conn = new PDO($conexao,"root","");
 			self::$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			self::$conn->exec("set names utf8");
 			return self::$conn;
